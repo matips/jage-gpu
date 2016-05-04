3a69c74ed730f8516f34ae4e67df34c5708846c4
Pszczolka, Mateusz <Mateusz.Pszczolka@sabre.com>
2016-05-04 11:10:35 +0200

    fix problem with invalid method

--- jage-gpu-examples/src/main/java/pl/edu/agh/jage/gpu/examples/adding/agents/GPUGridFragment.java
+++ jage-gpu-examples/src/main/java/pl/edu/agh/jage/gpu/examples/adding/agents/GPUGridFragment.java
@@ -4,13 +4,13 @@ import java.util.List;
 import java.util.Random;
 
 import org.jage.address.agent.AgentAddressSupplier;
+import org.jage.gpu.agent.GpuAgent;
 import org.jage.gpu.agent.SubStep;
-import org.jage.gpu.agent.SubStepAgent;
 import org.jage.gpu.executors.ExternalExecutor;
 
 import pl.edu.agh.jage.gpu.examples.IGridFragment;
 
-public class GPUGridFragment extends SubStepAgent implements IGridFragment {
+public class GPUGridFragment extends GpuAgent implements IGridFragment {
 
     private List<Integer> position;
     Random random = new Random();
